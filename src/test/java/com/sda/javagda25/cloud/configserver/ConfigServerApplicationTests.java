d52a675def83aa3e2e8419de55cae9d54c4d50e6
SzymonKuhn <szymon.kuhn@gmail.com>
2019-10-22 18:02:00 +0200

    first commit

---
+++ src/test/java/com/sda/javagda25/cloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javagda25.cloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
